a71510ca163b69521187f083b07503087ae281a3
kjun <leekj6101@gmail.com>
2026-03-24 21:41:38 +0900

    try useContext

--- week02/mission/src/App.tsx
+++ week02/mission/src/App.tsx
@@ -1,14 +1,8 @@
 import './App.css';
 import Todo from './components/Todo';
-import { TodoProvider } from './context/TodoContext';
-import { ThemeProvider } from './context/ThemeProvider';
 function App() {
   return (
-    <ThemeProvider>
-      <TodoProvider>
-        <Todo></Todo>
-      </TodoProvider>
-    </ThemeProvider>
+    <Todo></Todo>
   );
 }
 
