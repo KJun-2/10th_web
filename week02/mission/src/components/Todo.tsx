97817f5ce8f21ddc686dd265806a6e4e6ce53929
kjun <leekj6101@gmail.com>
2026-03-26 23:34:36 +0900

    commit

--- week02/mission/src/components/Todo.tsx
+++ week02/mission/src/components/Todo.tsx
@@ -1,12 +1,18 @@
-import clsx from 'clsx';
-import React from 'react';
-import ThemeToggleButton from '../context/ThemeToggleButton';
+import Button from './Button';
+import { THEME, useTheme } from '../context/ContextPractice';
 
+import clsx from 'clsx';
 function Todo() {
+  const { theme, toggleButton } = useTheme();
+  const isLight = theme === THEME.LIGHT;
   return (
-    <div className="container">
-      <div>Todo</div>
-      <ThemeToggleButton></ThemeToggleButton>
+    <div
+      className={clsx('items-center w-100 h-100  rounded-[10px]', {
+        'bg-black text-white border-black': isLight,
+        'bg-white text-black border-white': !isLight,
+      })}>
+      <h1 className={'text-center text-xl font-bold'}>My Text</h1>
+      <Button></Button>
     </div>
   );
 }
