0f6baad8547d20345c26fdf1b4d16edf9a54a4db
kjun <leekj6101@gmail.com>
2026-05-25 23:02:53 +0900

    init file

---
+++ week09/mission/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Navbar from './components/NavBar'
+import CartList from './components/CarList'
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <CartList />
+    </>
+  )
+}
+
+export default App
---
+++ week09/mission/src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+function NavBar() {
+    return (
+        <div className="flex justify-between items-center p-4 bg-gray-800">
+            <h1 className="text-white text-2xl font-bold">NavBar</h1>
+        </div>
+    )
+}
+
+export default NavBar
